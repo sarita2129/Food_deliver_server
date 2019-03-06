f71b0fc7b8f51ed4f17745d2df0eeea845a3f27b
sarita2129 <nairsarita2129@gmail.com>
2019-03-06 14:15:07 +1100

    final test

--- server.js
+++ server.js
@@ -16,25 +16,27 @@ var app = express();
 //   origin:"http://localhost:3000",
 //   credentials: true
 // }));
+//commented
+// app.use(function (req, res, next) {
+//
+//     // Website you wish to allow to connect
+//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+//
+//     // Request methods you wish to allow
+//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//
+//     // Request headers you wish to allow
+//     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+//
+//     // Set to true if you need the website to include cookies in the requests sent
+//     // to the API (e.g. in case you use sessions)
+//     res.setHeader('Access-Control-Allow-Credentials', true);
+//
+//     // Pass to next layer of middleware
+//     next();
+// });
+//commented
 
-app.use(function (req, res, next) {
-
-    // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-
-    // Request methods you wish to allow
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-    // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-    // Set to true if you need the website to include cookies in the requests sent
-    // to the API (e.g. in case you use sessions)
-    res.setHeader('Access-Control-Allow-Credentials', true);
-
-    // Pass to next layer of middleware
-    next();
-});
 app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.engine("html", require("ejs").renderFile);
@@ -44,7 +46,9 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use('/api', users);
 app.use('/api', restaurants);
 app.use('/api', orders);
-
+app.get('/', (req,res) => {
+  res.sendFile(path.join(__dirname + '/index.html'))
+});
 
 // app.use('/api/login', require('./routes/api/signin'));
 app.listen(PORT);
